b4c459119d9924666643f333631e56bdcae17121
Trexy94 <trexy94@live.it>
2016-08-25 10:13:45 +0200

    first upload

---
+++ background.js
@@ -0,0 +1,56 @@
+var index=0;
+var imglist=[];
+chrome.commands.onCommand.addListener(function(command) { 
+    chrome.storage.sync.get("data", function(obj) {
+    if (!chrome.runtime.error) {
+      alert(obj.data);
+    }
+  });
+  if(command === "toggle") {
+        alert(index);
+          for (i=0;i<index; i++)
+      {   
+          window.open(imglist[i]); 
+
+      }
+  }
+});
+  
+// Listen for a click on the camera icon. On that click, take a screenshot.
+chrome.browserAction.onClicked.addListener(function() {
+     chrome.windows.create({type: "popup",state:"minimized"})
+     intervallo=0;
+    while(intervallo<30000000)//print x times, where x is the number of milliseconds/milleseconds for screenshot
+    {
+    setTimeout(function(){Loop()},intervallo);    
+    intervallo=intervallo+60000;
+}
+});
+
+function Loop (){
+      chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+          imglist[index]=screenshotUrl;
+          chrome.storage.sync.set({"data" : new Date().toJSON()}, function() {
+          index++;
+    if (chrome.runtime.error) {
+      console.log("Runtime error.");
+    }
+  });      
+}
+);};
+
+chrome.browserAction.onClicked.addListener(function() {
+      chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+window.open(screenshotUrl);
+}
+)}
+);
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+                for (i=text;i>0; i--)
+      {   
+          window.open(imglist[index-i]); 
+
+      }
+  });
